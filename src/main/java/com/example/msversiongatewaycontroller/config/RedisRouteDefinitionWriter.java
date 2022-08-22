8f0302f5d2f6b4cfdcc6feebd185855b8bc068c9
pivot <18865290907@163.com>
2022-08-22 10:44:15 +0800

    route info stored at redis

---
+++ src/main/java/com/example/msversiongatewaycontroller/config/RedisRouteDefinitionWriter.java
@@ -0,0 +1,2 @@
+package com.example.msversiongatewaycontroller.config;public class RedisRouteDefinitionRepository {
+}
---
+++ src/main/java/com/example/msversiongatewaycontroller/service/SysRouteConfService.java
@@ -0,0 +1,4 @@
+package com.example.msversiongatewaycontroller.service;
+
+public class SysRouteConfServiceImpl{
+}
