27ab2040b3a4d6c04c94a53fb9e3d868d8d36d8e
pivot <18865290907@163.com>
2022-08-23 16:45:56 +0800

    Route definition

--- src/main/java/com/example/msversiongatewaycontroller/config/RedisRouteDefinitionWriter.java
+++ src/main/java/com/example/msversiongatewaycontroller/config/RedisRouteDefinitionWriter.java
@@ -1,2 +1,51 @@
-package com.example.msversiongatewaycontroller.config;public class RedisRouteDefinitionRepository {
+package com.example.msversiongatewaycontroller.config;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.route.RouteDefinition;
+import org.springframework.cloud.gateway.route.RouteDefinitionRepository;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+@Slf4j
+@Component
+@AllArgsConstructor
+public class RedisRouteDefinitionWriter implements RouteDefinitionRepository {
+
+    private final RedisTemplate redisTemplate;
+    private static final String ROUTE_KEY = "gateway_route_key";
+    @Override
+    public Flux<RouteDefinition> getRouteDefinitions() {
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new Jackson2JsonRedisSerializer<>(RouteDefinition.class));
+        List<RouteDefinition> values = redisTemplate.opsForHash().values(ROUTE_KEY);
+        log.debug("redis中路由定义条数: {}, {}", values.size(), values);
+        return Flux.fromIterable(values);
+    }
+
+    @Override
+    public Mono<Void> save(Mono<RouteDefinition> route) {
+        return route.flatMap(r -> {
+            log.info("保存路由信息{}", r);
+            redisTemplate.setKeySerializer(new StringRedisSerializer());
+            redisTemplate.opsForHash().put(ROUTE_KEY, r.getId(), r);
+            return Mono.empty();
+        });
+    }
+
+    @Override
+    public Mono<Void> delete(Mono<String> routeId) {
+        routeId.subscribe(id -> {
+            log.info("删除路由信息{}", id);
+            redisTemplate.setKeySerializer(new StringRedisSerializer());
+            redisTemplate.opsForHash().delete(ROUTE_KEY, id);
+        });
+        return Mono.empty();
+    }
 }
--- src/main/java/com/example/msversiongatewaycontroller/service/SysRouteConfService.java
+++ src/main/java/com/example/msversiongatewaycontroller/service/SysRouteConfService.java
@@ -1,4 +1,27 @@
 package com.example.msversiongatewaycontroller.service;
 
-public class SysRouteConfServiceImpl{
+
+import com.example.msversiongatewaycontroller.model.SysRouteConf;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@AllArgsConstructor
+@Service
+public class SysRouteConfService {
+    private final RedisTemplate<String, String> redisTemplate;
+    private final ApplicationEventPublisher applicationEventPublisher;
+    private static final boolean STATUS_NORMAL = false;
+
+    public List<SysRouteConf> routes() {
+        SysRouteConf conf = new SysRouteConf();
+        conf.setDelFlag(STATUS_NORMAL);
+        return new ArrayList<SysRouteConf>();
+    }
 }
