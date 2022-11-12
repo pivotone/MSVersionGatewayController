fada87b3e6352631e3f446a0f0ff105ddc0567fc
HouShengming <18865290907@163.com>
2022-11-12 15:38:05 +0800

    add new function: subscribe service information and timer task to get new service name

--- src/main/java/com/example/msversiongatewaycontroller/task/ServiceGetTask.java
+++ src/main/java/com/example/msversiongatewaycontroller/task/ServiceGetTask.java
@@ -1,15 +1,12 @@
 package com.example.msversiongatewaycontroller.task;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.ResponseHandler;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
+import com.alibaba.cloud.nacos.NacosDiscoveryProperties;
+import com.alibaba.nacos.api.exception.NacosException;
+import com.alibaba.nacos.api.naming.NamingFactory;
+import com.alibaba.nacos.api.naming.NamingService;
+import com.alibaba.nacos.api.naming.listener.AbstractEventListener;
+import com.alibaba.nacos.api.naming.listener.Event;
+import com.alibaba.nacos.api.naming.listener.NamingEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
@@ -19,6 +16,9 @@ import org.springframework.stereotype.Component;
 import javax.annotation.Resource;
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.*;
 
 @Component
 public class ServiceGetTask {
@@ -28,40 +28,55 @@ public class ServiceGetTask {
     @Resource
     private DiscoveryClient client;
 
-    @Scheduled(initialDelay = 1000, fixedDelay = 300 * 1000)
-    public void scheduleServiceTask() throws IOException {
+    @Resource
+    private NacosDiscoveryProperties nacosDiscoveryProperties;
+
+    private final Map<String, Boolean> serviceMap = new ConcurrentHashMap<>();
+
+    @Scheduled(initialDelay = 1000, fixedDelay = 5 * 1000)
+    public void scheduleServiceTask() throws IOException, NacosException {
         List<String> serviceNameLists = client.getServices();
+        setFalse(serviceNameLists);
         LOGGER.info(serviceNameLists.toString());
-        String url = "http://39.104.112.98:8848/nacos/v1/ns/instance/list?serviceName=";
+        String serverAddr = "39.104.112.98:8848";
+        String namespace = "public";
+        Properties properties = new Properties();
+        properties.setProperty("serverAddr", serverAddr);
+        properties.setProperty("namespace", namespace);
+        NamingService naming = NamingFactory.createNamingService(properties);
         for(String name : serviceNameLists) {
-            LOGGER.info(name);
-            CloseableHttpClient httpClient = HttpClients.createDefault();
-            HttpGet get = new HttpGet(url + name);
-            ResponseHandler<String> handler = httpResponse -> {
-                int status = httpResponse.getStatusLine().getStatusCode();
-                if (status >= 200 && status < 300) {
-                    HttpEntity entity = httpResponse.getEntity();
-                    return entity != null ? EntityUtils.toString(entity) : null;
-                } else {
-                    throw new ClientProtocolException(
-                            "Unexpected response status: " + status);
-                }
-            };
-            String res = null;
-            try {
-                res = httpClient.execute(get, handler);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            } finally {
-                httpClient.close();
+            if(serviceMap.containsKey(name) && serviceMap.get(name)) {
+                continue;
             }
-            if(res == null)
-                throw new IOException("execute failed");
-            LOGGER.info(res);
-            JSONObject jsonRes = JSON.parseObject(res);
-            List<JSONObject> hotsLists = JSON.parseArray(jsonRes.getString("hosts")).toJavaList(JSONObject.class);
-            LOGGER.info(jsonRes.getString("hosts"));
-            LOGGER.info(hotsLists.get(0).getString("metadata"));
+            serviceMap.put(name, true);
+            Executor executor = new ThreadPoolExecutor(1, 1, 0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<Runnable>(),
+                    new ThreadFactory() {
+                        @Override
+                        public Thread newThread(Runnable r) {
+                            Thread thread = new Thread(r);
+                            thread.setName(name);
+                            return thread;
+                        }
+                    });
+            naming.subscribe(name, new AbstractEventListener() {
+                @Override
+                public Executor getExecutor() {
+                    return executor;
+                }
+
+                @Override
+                public void onEvent(Event event) {
+                    LOGGER.info("订阅到的服务：" + ((NamingEvent) event).getServiceName());
+                    LOGGER.info("订阅到的实例：" + ((NamingEvent) event).getInstances());
+                }
+            });
         }
+    }
+
+    private void setFalse(List<String> serviceNameLists) {
+        serviceMap.replaceAll((k, v) -> false);
+        for(String name : serviceNameLists)
+            if(serviceMap.containsKey(name))
+                serviceMap.put(name, true);
     }
 }
