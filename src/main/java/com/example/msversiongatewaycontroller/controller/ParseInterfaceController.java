50b8ed1d5f81a2b583723d1a86a93e647d309bca
pivotone <18865290907@163.com>
2023-02-26 17:57:04 +0800

    modify parse to patch swagger2 and swagger3

--- src/main/java/com/example/msversiongatewaycontroller/controller/ParseInterfaceController.java
+++ src/main/java/com/example/msversiongatewaycontroller/controller/ParseInterfaceController.java
@@ -1,6 +1,7 @@
 package com.example.msversiongatewaycontroller.controller;
 
 import com.alibaba.fastjson.JSON;
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.example.msversiongatewaycontroller.entity.MService;
 import com.example.msversiongatewaycontroller.entity.MServiceInterface;
 import com.example.msversiongatewaycontroller.entity.MServiceVersion;
@@ -69,7 +70,11 @@ public class ParseInterfaceController {
                 .build();
         Response response = client.newCall(request).execute();
         String docs = Objects.requireNonNull(response.body()).string();
-        parseToDataBase(docs);
+        boolean flag = false;
+        if(url.contains("/v2/")) {
+            flag = true;
+        }
+        parseToDataBase(docs, flag);
         return Mono.just(ResultUtils.success());
     }
 
@@ -105,7 +110,7 @@ public class ParseInterfaceController {
                             throw new RuntimeException(e);
                         }
                         try {
-                            parseToDataBase(s);
+                            parseToDataBase(s, false);
                         } catch (JsonProcessingException e) {
                             throw new RuntimeException(e);
                         } catch (InterruptedException e) {
@@ -124,7 +129,7 @@ public class ParseInterfaceController {
         return Mono.just(ResultUtils.success());
     }
 
-    private void parseToDataBase(String docs) throws JsonProcessingException, InterruptedException {
+    private void parseToDataBase(String docs, boolean flag) throws JsonProcessingException, InterruptedException {
         ObjectMapper mapper = new ObjectMapper();
         JsonNode node = mapper.readTree(docs);
         JsonNode info = node.get("info");
@@ -148,32 +153,41 @@ public class ParseInterfaceController {
         JsonNode paths = node.get("paths");
         Iterator<String> keys = paths.fieldNames();
         while(keys.hasNext()) {
-            threadPoolTaskExecutor.submit(new Thread(() -> {
-                String path = keys.next();
-                JsonNode pathNode = paths.get(path);
-                LOGGER.info("解析到的path路径为： " + path);
-                Iterator<String> methods = paths.get(path).fieldNames();
-                while(methods.hasNext()) {
-                    MServiceInterface serviceInterface = new MServiceInterface();
-                    serviceInterface.setApi(path);
-                    serviceInterface.setVersionId(versionId);
-                    String method = methods.next();
-                    LOGGER.info("解析到的method为：" + method);
+            String path = keys.next();
+            JsonNode pathNode = paths.get(path);
+            LOGGER.info("解析到的path路径为： " + path);
+            Iterator<String> methods = paths.get(path).fieldNames();
+            while(methods.hasNext()) {
+                MServiceInterface serviceInterface = new MServiceInterface();
+                serviceInterface.setApi(path);
+                serviceInterface.setVersionId(versionId);
+                String method = methods.next();
+                serviceInterface.setRequestType(method);
+                LOGGER.info("解析到的method为：" + method);
+                if(!flag) {
                     LOGGER.info("解析到的method向前兼容为： " +
                             pathNode.get(method).get("extensions").get("x-forward-compatible-marker").asText());
-                    serviceInterface.setRequestType(method);
                     serviceInterface.setMarker(Integer.parseInt(
                             pathNode.get(method).get("extensions").get("x-forward-compatible-marker").asText()
                     ));
-                    serviceInterface.setDescription(
-                            pathNode.get(method).get("summary").asText());
-                    MServiceInterface tempServiceInterface =
-                            mServiceInterfaceService.selectInterfaceByMethodAndApi(serviceInterface);
-                    if(tempServiceInterface == null) {
-                        mServiceInterfaceService.saveInterface(serviceInterface);
-                    }
+                } else {
+                    LOGGER.info("解析到的method向前兼容为： " +
+                            pathNode.get(method).get("x-forward-compatible-marker").asText());
+                    serviceInterface.setMarker(Integer.parseInt(
+                            pathNode.get(method).get("x-forward-compatible-marker").asText()
+                    ));
                 }
-            }));
+                serviceInterface.setDescription(
+                        pathNode.get(method).get("summary").asText());
+                MServiceInterface tempServiceInterface =
+                        mServiceInterfaceService.selectInterfaceByMethodAndApi(serviceInterface);
+                if(tempServiceInterface == null) {
+                    mServiceInterfaceService.saveInterface(serviceInterface);
+                } else {
+                    serviceInterface.setId(tempServiceInterface.getId());
+                    mServiceInterfaceService.updateById(serviceInterface);
+                }
+            }
         }
         Thread.sleep(1000);
     }
